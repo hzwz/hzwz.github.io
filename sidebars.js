fe4ed491a261f6db4a2a8e7d772a5f2962e3512a
ATOMIC <21135655+hzwz@users.noreply.github.com>
2023-06-06 23:49:51 +0800

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -4,7 +4,7 @@
  * @LastEditors: root root@gmail.com
  * @LastEditTime: 2023-06-06 18:16:56
  * @FilePath: /hzwz.github.io/sidebars.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 // @ts-check
 
